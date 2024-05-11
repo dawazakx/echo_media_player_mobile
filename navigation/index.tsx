0949bafe7511ae9f6875ef7a2912fc8a929c54d7
Habeeb Mac-iver <hmaciver@agilenceince.com>
2024-05-11 10:24:37 +0100

    Navigation setup

---
+++ navigation/index.tsx
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "../screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ screens/Home.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const HomeScreen = () => {
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default HomeScreen;
