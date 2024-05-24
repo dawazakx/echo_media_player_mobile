56ee06ecbe8e7e0f90b809fbb53769bc360de74c
david2tosin <david2tosin@gmail.com>
2024-05-24 15:07:51 +0100

    test

--- screens/tabs/MoviesTab.tsx
+++ screens/tabs/MoviesTab.tsx
@@ -1,10 +1,12 @@
 import { CustomText } from "@/components/Text";
 import { CustomView } from "@/components/View";
 import { Colors } from "@/constants/Colors";
+import { BASE_URL } from "@/constants/api";
 import { TabParamList } from "@/constants/types";
+import { DeviceContext } from "@/providers/DeviceProvider";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { RouteProp } from "@react-navigation/native";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 export interface MoviesProps {
   navigation: BottomTabScreenProps<TabParamList, "Movies">;
@@ -12,6 +14,56 @@ export interface MoviesProps {
 }
 
 const MoviesTab: React.FC<MoviesProps> = ({ navigation, route }) => {
+  const deviceId = useContext(DeviceContext);
+  // console.log(deviceId);
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!deviceId || typeof deviceId !== "string") {
+        throw new Error("Invalid device ID");
+      }
+
+      try {
+        const response = await fetch(`${BASE_URL}vod-stream-category`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            device_id: deviceId,
+          },
+        });
+
+        // if (!response.ok) {
+        //   throw new Error(
+        //     `Network response was not ok: ${response.statusText}`
+        //   );
+        // }
+
+        const jsonData = await response.json();
+        console.log(jsonData);
+        // setData(jsonData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Fetch error:", error);
+        setError(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [deviceId]);
+
+  if (isLoading)
+    return <CustomText style={{ color: "black" }}>Loading...</CustomText>;
+  if (error)
+    return (
+      <CustomText style={{ color: "black" }}>Error: {error.message}</CustomText>
+    );
+
+  console.log("API Response:", data);
+
   return (
     <CustomView
       style={{
