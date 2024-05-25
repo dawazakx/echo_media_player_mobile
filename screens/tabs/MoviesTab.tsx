cb1a77a20a755d05514488556534e74e6b0e5546
david2tosin <david2tosin@gmail.com>
2024-05-25 15:13:06 +0100

    test

--- screens/tabs/MoviesTab.tsx
+++ screens/tabs/MoviesTab.tsx
@@ -1,3 +1,5 @@
+import HorizontalFilter from "@/components/HorizontalFilter";
+import MovieCarousel from "@/components/MovieCarousel";
 import { CustomText } from "@/components/Text";
 import { CustomView } from "@/components/View";
 import { Colors } from "@/constants/Colors";
@@ -6,77 +8,213 @@ import { TabParamList } from "@/constants/types";
 import { DeviceContext } from "@/providers/DeviceProvider";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { RouteProp } from "@react-navigation/native";
+import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  Pressable,
+  ScrollView,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export interface MoviesProps {
   navigation: BottomTabScreenProps<TabParamList, "Movies">;
   route: RouteProp<TabParamList, "Movies">;
 }
 
+export interface Category {
+  category_id: string;
+  category_name: string;
+  parent_id: number;
+  // streams: Movie[];
+}
+
+export type Movie = {
+  added: Date;
+  category_id: string;
+  category_name: string;
+  container_extension: string;
+  custom_sid: string;
+  direct_source: string;
+  epg_channel_id: string;
+  stream_icon: string;
+  stream_id: number;
+  url: string;
+  name: string;
+  num: number;
+  rating: number;
+  rating_5based: number;
+  tv_archive: number;
+  tv_archive_duration: number;
+  stream_type: string;
+};
+
+const fetchCategories = async (
+  deviceId: string | null
+): Promise<Category[]> => {
+  if (!deviceId || typeof deviceId !== "string") {
+    throw new Error("Invalid device ID");
+  }
+  try {
+    const response = await axios.get(`${BASE_URL}vod-stream-category`, {
+      headers: {
+        "Content-Type": "application/json",
+        "device-id": deviceId,
+      },
+    });
+    return response.data.vodCategories;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+const fetchMoviesByCategory = async (
+  deviceId: string | null,
+  categoryId: string
+): Promise<Movie[]> => {
+  if (!deviceId || typeof deviceId !== "string") {
+    throw new Error("Invalid device ID");
+  }
+  try {
+    const response = await axios.get(
+      `${BASE_URL}vod-stream?category_id=${categoryId}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "device-id": deviceId,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
 const MoviesTab: React.FC<MoviesProps> = ({ navigation, route }) => {
-  const deviceId = useContext(DeviceContext);
-  // console.log(deviceId);
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { deviceId } = useContext(DeviceContext);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      if (!deviceId || typeof deviceId !== "string") {
-        throw new Error("Invalid device ID");
-      }
-
-      try {
-        const response = await fetch(`${BASE_URL}vod-stream-category`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            device_id: deviceId,
-          },
-        });
-
-        // if (!response.ok) {
-        //   throw new Error(
-        //     `Network response was not ok: ${response.statusText}`
-        //   );
-        // }
-
-        const jsonData = await response.json();
-        console.log(jsonData);
-        // setData(jsonData);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Fetch error:", error);
-        setError(error);
-        setIsLoading(false);
+      const categoriesData = await fetchCategories(deviceId);
+      // console.log("Categories fetched:", categoriesData);
+      setCategories(categoriesData);
+      if (categoriesData.length > 0) {
+        setSelectedCategory(categoriesData[0].category_id);
       }
     };
-
     fetchData();
   }, [deviceId]);
 
-  if (isLoading)
-    return <CustomText style={{ color: "black" }}>Loading...</CustomText>;
-  if (error)
-    return (
-      <CustomText style={{ color: "black" }}>Error: {error.message}</CustomText>
-    );
-
-  console.log("API Response:", data);
+  useEffect(() => {
+    const fetchMovies = async () => {
+      if (selectedCategory) {
+        const moviesData = await fetchMoviesByCategory(
+          deviceId,
+          selectedCategory
+        );
+        console.log(
+          `Movies for category ${selectedCategory} fetched:`,
+          moviesData
+        );
+        setMovies(moviesData);
+      }
+    };
+    fetchMovies();
+  }, [selectedCategory, deviceId]);
 
   return (
-    <CustomView
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: Colors.background,
-        height: "100%",
-      }}
-    >
-      <CustomText type="title" style={{ textAlign: "center" }}>
-        Movies Tab
-      </CustomText>
-    </CustomView>
+    <View style={styles.container}>
+      <FlatList
+        data={categories}
+        horizontal
+        keyExtractor={(item) => item.category_id}
+        renderItem={({ item }) => (
+          <Pressable
+            style={[
+              styles.categoryButton,
+              item.category_id === selectedCategory &&
+                styles.selectedCategoryButton,
+            ]}
+            onPress={() => setSelectedCategory(item.category_id)}
+          >
+            <Text style={styles.categoryText}>{item.category_name}</Text>
+          </Pressable>
+        )}
+      />
+
+      <ScrollView>
+        {selectedCategory && (
+          <View key={selectedCategory} style={styles.categorySection}>
+            <Text style={styles.categoryTitle}>
+              {
+                categories.find((cat) => cat.category_id === selectedCategory)
+                  ?.category_name
+              }
+            </Text>
+            <FlatList
+              data={movies}
+              horizontal
+              keyExtractor={(item) => item.stream_id.toString()}
+              renderItem={({ item }) => (
+                <View style={styles.movieItem}>
+                  <Image
+                    source={{ uri: item.stream_icon }}
+                    style={styles.movieImage}
+                  />
+                </View>
+              )}
+            />
+          </View>
+        )}
+      </ScrollView>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  categoryButton: {
+    padding: 10,
+    marginHorizontal: 5,
+    backgroundColor: "#ddd",
+    borderRadius: 20,
+  },
+  selectedCategoryButton: {
+    backgroundColor: "#007bff",
+  },
+  categoryText: {
+    color: "#000",
+  },
+  categorySection: {
+    marginVertical: 10,
+  },
+  categoryTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 10,
+  },
+  movieItem: {
+    marginHorizontal: 10,
+  },
+  movieImage: {
+    width: 100,
+    height: 150,
+    borderRadius: 10,
+  },
+});
+
 export default MoviesTab;
