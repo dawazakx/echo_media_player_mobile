04e14664f0bbdd71f6fd1ffb90b9afd71ba91d73
Habeeb Mac-iver <hmaciver@agilenceince.com>
2024-05-28 13:39:47 +0100

    WIP

--- providers/api.ts
+++ providers/api.ts
@@ -2,7 +2,59 @@ import axios from "axios";
 
 import { BASE_URL } from "@/constants/api";
 
-import { type Category, type Movie } from "@/types";
+import { type Category, type LiveStream } from "@/types";
+
+export const fetchLiveStreamCategories = async (
+  deviceId: string | null
+): Promise<Category[]> => {
+  if (!deviceId || typeof deviceId !== "string") {
+    throw new Error("Invalid device ID");
+  }
+  try {
+    const response = await axios.get(`${BASE_URL}live-stream-category`, {
+      headers: {
+        "Content-Type": "application/json",
+        "device-id": deviceId,
+      },
+    });
+    return response.data.categories;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const fetchLiveStreams = async (
+  deviceId: string | null,
+  categories: Category[]
+): Promise<{ [key: string]: LiveStream[] }> => {
+  try {
+    const allStreams = await Promise.all(
+      categories.map(async (category) => {
+        const response = await axios.get(
+          `${BASE_URL}live-stream?category_id=${category.category_id}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "device-id": deviceId,
+            },
+          }
+        );
+        return {
+          [category.category_id]: {
+            ...response.data.streams,
+            category_name: category.category_name,
+          },
+        };
+      })
+    );
+
+    return allStreams.reduce((acc, curr) => ({ ...acc, ...curr }), {});
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
+};
 
 export const fetchCategories = async (
   deviceId: string | null
--- screens/tabs/LiveTvTab.tsx
+++ screens/tabs/LiveTvTab.tsx
@@ -1,11 +1,22 @@
-import { CustomText } from "@/components/Text";
-import { CustomView } from "@/components/View";
-import { Colors } from "@/constants/Colors";
-import { TabParamList } from "@/constants/types";
+import React, { useState, useContext, useRef, useEffect } from "react";
+
+import { StyleSheet, ActivityIndicator, FlatList, } from "react-native";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { RouteProp } from "@react-navigation/native";
-import React from "react";
-import { StyleSheet } from "react-native";
+
+import { DeviceContext } from "@/providers/DeviceProvider";
+
+import { CustomText } from "@/components/Text";
+import { CustomView } from "@/components/View";
+import CategoryFilter from "@/components/CategoryFilter";
+import LiveStreamCategoryGroup from "@/components/LiveStreamCategoryGroup";
+
+import { Colors } from "@/constants/Colors";
+import { TabParamList } from "@/constants/types";
+
+import { fetchLiveStreamCategories, fetchLiveStreams } from "@/providers/api";
+
+import { Category, LiveStream } from "@/types";
 
 export interface LiveTvProps {
   navigation: BottomTabScreenProps<TabParamList, "LiveTV">;
@@ -13,20 +24,83 @@ export interface LiveTvProps {
 }
 
 const LiveTvTab: React.FC<LiveTvProps> = ({ navigation, route }) => {
+  const { deviceId } = useContext(DeviceContext);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [liveStreams, setLiveStreams] = useState<{ [key: string]: LiveStream[] }>({});
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const flatListRef = useRef<FlatList>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+  
+      const categoriesData = await fetchLiveStreamCategories(deviceId);
+      const availableCategories = categoriesData.slice(0, 30);
+      setCategories(availableCategories);
+  
+      const liveStreamData = await fetchLiveStreams(deviceId, availableCategories);
+      setLiveStreams(liveStreamData);
+  
+      if (categoriesData.length > 0) {
+        setSelectedCategory(categoriesData[0].category_id);
+      }
+  
+      setLoading(false);
+    };
+
+    fetchData();
+  }, [deviceId]);
+
+  const handleCategoryPress = (categoryId: string) => {
+    setSelectedCategory(categoryId);
+    const categoryIndex = categories.findIndex(
+      (category) => category.category_id === categoryId
+    );
+    if (flatListRef.current) {
+      flatListRef.current.scrollToIndex({
+        index: categoryIndex,
+        animated: true,
+      });
+    }
+  };
+
+  if (loading) {
+    return (
+      <CustomView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color={Colors.white} />
+      </CustomView>
+    );
+  }
+
+  if (categories.length === 0) {
+    return (
+      <CustomView style={styles.container}>
+        <CustomText>No categories found.</CustomText>
+      </CustomView>
+    );
+  }
+
   return (
-    <CustomView
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: Colors.background,
-        height: "100%",
-      }}
-    >
-      <CustomText type="title" style={{ textAlign: "center" }}>
-        LiveTV Tab
-      </CustomText>
+    <CustomView style={styles.container}>
+      <CategoryFilter categories={categories} selectedCategory={selectedCategory} onSelect={handleCategoryPress} />
+
+      <LiveStreamCategoryGroup navigation={navigation} categories={categories} streams={liveStreams} flatListRef={flatListRef} />
     </CustomView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.secBackground,
+  },
+  
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.secBackground,
+  },
+});
+
 export default LiveTvTab;
-const styles = StyleSheet.create({});
