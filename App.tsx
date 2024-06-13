7aad8c3d7907b4eff030319070f3739d2933d9ba
Habeeb Mac-iver <hmaciver@agilenceince.com>
2024-06-13 16:26:33 +0100

    Added redux to the project

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
+import React from 'react';
+
 import AppNavigator from './navigation';
+
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 import { DeviceProvider } from './providers/DeviceProvider';
 import { PlaylistProvider } from './providers/PlaylistProvider';
 
 export default function App() {
   return (
-    <DeviceProvider>
-      <PlaylistProvider>
-        <AppNavigator />
-      </PlaylistProvider>
-    </DeviceProvider>
+    <Provider store={store}>
+      <DeviceProvider>
+        <PlaylistProvider>
+          <AppNavigator />
+        </PlaylistProvider>
+      </DeviceProvider>
+    </Provider>
   );
 }
